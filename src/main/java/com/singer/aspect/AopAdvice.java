b0768624b2f35ad33cd034bc00c02f3204184f29
Insung Choi <choiis1207@gmail.com>
2019-07-10 00:02:39 +0900

    20190710 Redis 연결방법 바꿈

--- src/main/java/com/singer/aspect/AopAdvice.java
+++ src/main/java/com/singer/aspect/AopAdvice.java
@@ -2,6 +2,8 @@ package com.singer.aspect;
 
 import java.util.Arrays;
 
+import javax.annotation.Resource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
@@ -11,11 +13,14 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import com.singer.common.DateUtil;
+import com.singer.dao.ErrorDao;
+import com.singer.vo.ErrorVo;
 
 @Component
 @Aspect
@@ -25,7 +30,6 @@ public class AopAdvice {
 	@After("execution(* com.singer.service.*Impl.*(..))")
 	public void aopService(JoinPoint joinPoint) {
 
-		log.debug("aopService============");
 		log.debug(Arrays.toString(joinPoint.getArgs()));
 		Signature sg = joinPoint.getSignature();
 		log.debug(sg.getName());
@@ -36,7 +40,6 @@ public class AopAdvice {
 	@Around("execution(* com.singer.dao.*Dao.*(..))")
 	public Object aopDao(ProceedingJoinPoint pjp) throws Throwable {
 
-		log.debug("aopDao============");
 		log.debug(Arrays.toString(pjp.getArgs()));
 
 		long startTime = System.currentTimeMillis();
@@ -51,13 +54,22 @@ public class AopAdvice {
 	@Autowired
 	RedisTemplate<String, Object> redisTemplate;
 
-	@After("execution(* com.singer.controller.*Controller.show*(..))")
-	public void aopShow(JoinPoint pjp) throws Throwable {
+	@Resource(name = "errorDao")
+	private ErrorDao errorDao;
 
-		log.debug("aopShow============");
-		// Redis Insert
-		// Hash구조에 Key는 날짜 field는 화면명
-		HashOperations<String, String, Object> hashOp = redisTemplate.opsForHash();
-		hashOp.increment(DateUtil.getToday(), pjp.getSignature().getName(), 1);
+	@After("execution(* com.singer.controller.*Controller.show*(..))")
+	public void aopShow(JoinPoint pjp) {
+		try {
+			// Redis Insert
+			// Hash구조에 Key는 날짜 field는 화면명
+			HashOperations<String, String, Object> hashOp = redisTemplate.opsForHash();
+			hashOp.increment(DateUtil.getToday(), pjp.getSignature().getName(), 1);
+		} catch (RedisConnectionFailureException e) {
+			log.debug("RedisConnectionFailureException");
+			ErrorVo errorVo = new ErrorVo(pjp.getSignature().getName(), DateUtil.getTodayTime(),
+					"RedisConnectionFailureException");
+			errorDao.insertError(errorVo);
+		}
+
 	}
 }
--- src/main/java/com/singer/redis/RedisConfig.java
+++ src/main/java/com/singer/redis/RedisConfig.java
@@ -2,6 +2,7 @@ package com.singer.redis;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -11,33 +12,21 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-	private String server = "127.0.0.1";
-	private int port = 6379;
-
 	private final Log log = LogFactory.getLog(RedisConfig.class);
 
-	@Bean
-	public JedisConnectionFactory connectionFactory() {
-		log.debug("HostName : " + server);
-		log.debug("port : " + port);
-
-		JedisConnectionFactory conn = new JedisConnectionFactory();
-		conn.setHostName(server);
-		conn.setPort(port);
-
-		conn.setUsePool(true);
-		return conn;
-	}
+	@Autowired
+	private JedisConnectionFactory conn;
 
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate() {
+		log.debug("Redis Serializer");
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
 		StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
 		redisTemplate.setKeySerializer(stringRedisSerializer);
 		redisTemplate.setValueSerializer(stringRedisSerializer);
 		redisTemplate.setHashKeySerializer(stringRedisSerializer);
 		redisTemplate.setHashValueSerializer(stringRedisSerializer);
-		redisTemplate.setConnectionFactory(connectionFactory());
+		redisTemplate.setConnectionFactory(conn);
 		return redisTemplate;
 	}
 
