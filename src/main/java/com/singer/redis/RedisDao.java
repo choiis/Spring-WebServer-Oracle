e9919a5d6101a20db4691bf513ee770c37dff27f
Insung Choi <choiis1207@gmail.com>
2019-07-25 23:01:24 +0900

    20190725 Redis 조회 메서드 추가

--- src/main/java/com/singer/redis/RedisDao.java
+++ src/main/java/com/singer/redis/RedisDao.java
@@ -1,5 +1,9 @@
 package com.singer.redis;
 
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Vector;
+
 import javax.annotation.Resource;
 
 import org.apache.commons.logging.Log;
@@ -9,11 +13,13 @@ import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 import org.springframework.stereotype.Repository;
 
 import com.singer.common.DateUtil;
 import com.singer.dao.ErrorDao;
 import com.singer.vo.ErrorVo;
+import com.singer.vo.SortedSetVo;
 
 @Repository("redisDao")
 public class RedisDao {
@@ -69,6 +75,34 @@ public class RedisDao {
 			log.debug("RedisConnectionFailureException");
 			ErrorVo errorVo = new ErrorVo(field, DateUtil.getTodayTime(), "RedisConnectionFailureException");
 			errorDao.insertError(errorVo);
+		}
+	}
+
+	// hmget key value
+	public Integer getHashValue(String key, String hashKey) {
+
+		HashOperations<String, String, Object> hashOp = redisTemplate.opsForHash();
+		try {
+
+			int result = Integer.parseInt((String) hashOp.get(key, hashKey));
+			return result;
+		} catch (Exception e) {
+			return 0;
+		}
+
+	}
+
+	// zrevrange key 0 -1 withscores
+	public void getSortedSet(String key) {
+
+		ZSetOperations<String, Object> zsetOp = redisTemplate.opsForZSet();
+		Set<TypedTuple<Object>> sortedSet = zsetOp.reverseRangeWithScores(key, 0, -1);
+
+		Iterator<TypedTuple<Object>> iter = sortedSet.iterator();
+		Vector<SortedSetVo> vector = new Vector<SortedSetVo>();
+		while (iter.hasNext()) {
+			TypedTuple<Object> typedTuple = iter.next();
+			vector.add(new SortedSetVo((String) typedTuple.getValue(), typedTuple.getScore()));
 		}
 	}
 }
