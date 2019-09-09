a15935641ad5514a2f1514bf6d464ea1a07a7f58
최인성 <choiis1207@gmail.com>
2019-09-09 22:09:56 +0900

    20190909 Table구조 ;빠진것 추가

--- Table구조.sql
+++ Table구조.sql
@@ -1,4 +1,4 @@
-CREATE table SM01(
+﻿CREATE table SM01(
 userid varchar2(10) not null,
 passwd varchar2(30) not null,
 adminyn varchar2(5) not null,
@@ -30,14 +30,14 @@ CREATE table MENU (
 alter table MENU
 add constraint pk_MENU primary key(menucd);
 
-insert into MENU values('01','코드관리','/codepage','01','admin','20180901','admin','20180901')
-insert into MENU values('02','공지사항','/commandpage','02','admin','20180901','admin','20180901')
-insert into MENU values('03','회원관리','/sm01page','02','admin','20180901','admin','20180901')
-insert into MENU values('04','노래 동영상','/sb01page','04','admin','20180901','admin','20180901')
-insert into MENU values('05','파일 게시판','/sf01page','04','admin','20180901','admin','20180901')
-insert into MENU values('06','상품 거래','/sp01page','04','admin','20180901','admin','20180901')
-insert into MENU values('07','투표 게시판','/sv01page','04','admin','20180901','admin','20180901')
-insert into MENU values('08','나의 메모장','/sm02page','04','admin','20180901','admin','20180901')
+insert into MENU values('01','코드관리','/codepage','01','admin','20180901','admin','20180901');
+insert into MENU values('02','공지사항','/commandpage','02','admin','20180901','admin','20180901');
+insert into MENU values('03','회원관리','/sm01page','02','admin','20180901','admin','20180901');
+insert into MENU values('04','노래 동영상','/sb01page','04','admin','20180901','admin','20180901');
+insert into MENU values('05','파일 게시판','/sf01page','04','admin','20180901','admin','20180901');
+insert into MENU values('06','상품 거래','/sp01page','04','admin','20180901','admin','20180901');
+insert into MENU values('07','투표 게시판','/sv01page','04','admin','20180901','admin','20180901');
+insert into MENU values('08','나의 메모장','/sm02page','04','admin','20180901','admin','20180901');
 
 CREATE table CODE_GRP (
 codegrp varchar2(5) not null,
@@ -64,32 +64,32 @@ alter table CODE
 add constraint fk_code foreign key(codegrp) references CODE_GRP(codegrp) on delete cascade;
 
 
-insert into CODE_GRP values('U001','유저등급코드','admin','20180901')
-insert into CODE_GRP values('P001','판매상태코드','admin','20180901')
-insert into CODE_GRP values('P002','판매상품코드','admin','20180901')
+insert into CODE_GRP values('U001','유저등급코드','admin','20180901');
+insert into CODE_GRP values('P001','판매상태코드','admin','20180901');
+insert into CODE_GRP values('P002','판매상품코드','admin','20180901');
 
-insert into CODE values('01','관리자','U001','admin','20180901')
-insert into CODE values('02','특별회원','U001','admin','20180901')
-insert into CODE values('03','우수회원','U001','admin','20180901')
-insert into CODE values('04','사용자','U001','admin','20180901')
+insert into CODE values('01','관리자','U001','admin','20180901');
+insert into CODE values('02','특별회원','U001','admin','20180901');
+insert into CODE values('03','우수회원','U001','admin','20180901');
+insert into CODE values('04','사용자','U001','admin','20180901');
 
-insert into CODE values('01','판매대기','P001','admin','20180901')
-insert into CODE values('02','판매신청','P001','admin','20180901')
-insert into CODE values('03','판매완료','P001','admin','20180901')
+insert into CODE values('01','판매대기','P001','admin','20180901');
+insert into CODE values('02','판매신청','P001','admin','20180901');
+insert into CODE values('03','판매완료','P001','admin','20180901');
 
-insert into CODE values('01','음반','P002','admin','20180901')
-insert into CODE values('02','악보','P002','admin','20180901')
-insert into CODE values('03','악기','P002','admin','20180901')
-insert into CODE values('04','티켓','P002','admin','20180901')
-insert into CODE values('05','파일','P002','admin','20180901')
-insert into CODE values('06','기타','P002','admin','20180901')
+insert into CODE values('01','음반','P002','admin','20180901');
+insert into CODE values('02','악보','P002','admin','20180901');
+insert into CODE values('03','악기','P002','admin','20180901');
+insert into CODE values('04','티켓','P002','admin','20180901');
+insert into CODE values('05','파일','P002','admin','20180901');
+insert into CODE values('06','기타','P002','admin','20180901');
 
 CREATE table SL01(
 userid varchar2(10) not null,
 logintime varchar2(20) not null,
 browser varchar2(20) not null,
 device varchar2(20) not null,
-ip varchar2(30) 
+ip varchar2(30)
 );
 
 alter table SL01
@@ -116,7 +116,7 @@ add constraint fk_sm02 foreign key(userid) references SM01(userid) on delete cas
 CREATE index idx_SM02_1
 on SM02(userid, regdate);
 
-CREATE SEQUENCE seq_SB01 
+CREATE SEQUENCE seq_SB01
 START WITH 1 INCREMENT BY 1 ;
 
 create table SB01(
@@ -131,7 +131,7 @@ create table SB01(
 );
 
 alter table SB01
-add constraint pk_SB01 primary key(seq)
+add constraint pk_SB01 primary key(seq);
 
 alter table SB01
 add constraint fk_sb01 foreign key(userid) references SM01(userid) on delete cascade;
@@ -145,7 +145,7 @@ on SB01(title);
 CREATE index idx_SB01_3
 on SB01(userid);
 
-CREATE SEQUENCE seq_SB02 
+CREATE SEQUENCE seq_SB02
 START WITH 1 INCREMENT BY 1 ;
 
 create table SB02(
@@ -158,7 +158,7 @@ create table SB02(
 );
 
 alter table SB02
-add constraint pk_SB02 primary key(seq, seq01)
+add constraint pk_SB02 primary key(seq, seq01);
 
 alter table SB02
 add constraint fk_sb02 foreign key(seq01) references SB01(seq) on delete cascade;
@@ -171,7 +171,7 @@ create table SBG1(
   sessionid varchar2(20) not null,
   datelog varchar2(20) not null,
   goodlog varchar2(3),
-  hatelog varchar2(3) 
+  hatelog varchar2(3)
 );
 
 alter table SBG1
@@ -180,7 +180,7 @@ add constraint pk_SBG1 primary key(seq,sessionid);
 alter table SBG1
 add constraint fk_sbg1 foreign key(seq) references SB01(seq) on delete cascade;
 
-CREATE SEQUENCE seq_SF01 
+CREATE SEQUENCE seq_SF01
 START WITH 1 INCREMENT BY 1 ;
 
 create table SF01(
@@ -196,7 +196,7 @@ create table SF01(
 );
 
 alter table SF01
-add constraint pk_SF01 primary key(seq)
+add constraint pk_SF01 primary key(seq);
 
 alter table SF01
 add constraint fk_sf01 foreign key(userid) references SM01(userid) on delete cascade;
@@ -211,7 +211,7 @@ CREATE index idx_SF01_3
 on SF01(userid);
 
 
-CREATE SEQUENCE seq_SF02 
+CREATE SEQUENCE seq_SF02
 START WITH 1 INCREMENT BY 1 ;
 
 create table SF02(
@@ -224,7 +224,7 @@ create table SF02(
 );
 
 alter table SF02
-add constraint pk_SF02 primary key(seq, seq01)
+add constraint pk_SF02 primary key(seq, seq01);
 
 alter table SF02
 add constraint fk_sf02 foreign key(seq01) references SF01(seq) on delete cascade;
@@ -237,7 +237,7 @@ create table SFG1(
   sessionid varchar2(20) not null,
   datelog varchar2(20) not null,
   goodlog varchar2(3),
-  hatelog varchar2(3) 
+  hatelog varchar2(3)
 );
 
 alter table SFG1
@@ -246,7 +246,7 @@ add constraint pk_SFG1 primary key(seq,sessionid);
 alter table SFG1
 add constraint fk_sfg1 foreign key(seq) references SF01(seq) on delete cascade;
 
-CREATE SEQUENCE seq_SP01 
+CREATE SEQUENCE seq_SP01
 START WITH 1 INCREMENT BY 1 ;
 
 create table SP01(
@@ -263,7 +263,7 @@ create table SP01(
 );
 
 alter table SP01
-add constraint pk_SP01 primary key(seq)
+add constraint pk_SP01 primary key(seq);
 
 alter table SP01
 add constraint fk_sp01 foreign key(userid) references SM01(userid) on delete cascade;
@@ -277,7 +277,7 @@ on SP01(title);
 CREATE index idx_SP01_3
 on SP01(userid);
 
-CREATE SEQUENCE seq_SP02 
+CREATE SEQUENCE seq_SP02
 START WITH 1 INCREMENT BY 1 ;
 
 create table SP02(
@@ -290,7 +290,7 @@ create table SP02(
 );
 
 alter table SP02
-add constraint pk_SP02 primary key(seq, seq01)
+add constraint pk_SP02 primary key(seq, seq01);
 
 alter table SP02
 add constraint fk_sp02 foreign key(seq01) references SP01(seq) on delete cascade;
@@ -305,7 +305,7 @@ create table SP03(
 );
 
 alter table SP03
-add constraint pk_SP03 primary key(registerid, seq)
+add constraint pk_SP03 primary key(registerid, seq);
 
 alter table SP03
 add constraint fk_sp03 foreign key(seq) references SP01(seq) on delete cascade;
@@ -315,7 +315,7 @@ create table SPG1(
   sessionid varchar2(20) not null,
   datelog varchar2(20) not null,
   goodlog varchar2(3),
-  hatelog varchar2(3) 
+  hatelog varchar2(3)
 );
 
 alter table SPG1
@@ -324,7 +324,7 @@ add constraint pk_SPG1 primary key(seq,sessionid);
 alter table SPG1
 add constraint fk_spg1 foreign key(seq) references SP01(seq) on delete cascade;
 
-CREATE SEQUENCE seq_SV01 
+CREATE SEQUENCE seq_SV01
 START WITH 1 INCREMENT BY 1 ;
 
 create table SV01(
