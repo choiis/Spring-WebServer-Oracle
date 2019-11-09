df4f54d0fb97c4f27104afa1b45b1ad12af2ec75
최인성 <choiis1207@gmail.com>
2019-11-09 15:21:02 +0900

    20191109

---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+# Spring-Web
+
+## Tools
+Java Spring Oracle Redis Kafka JQuery
+
+### Tomcat Config
+
+<Connector connectionTimeout="20000" 
+    		port="6789" 
+    		disableUploadTimeout="true"
+    		protocol="HTTP/1.1" 
+    		maxSpareThreads="50"    
+    		maxThreads="300"
+    		minSpareThreads="25"
+    		acceptCount="10"
+    		tcpNoDelay="true"/>
+
+<Context docBase="Singer" path="/" reloadable="true" source="org.eclipse.jst.jee.server:Singer"  allowCasualMultipartParsing="true"/>
--- src/main/java/com/singer/exception/CommonExceptionHandler.java
+++ src/main/java/com/singer/exception/CommonExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import com.singer.common.CommonUtil;
 import com.singer.common.DateUtil;
 import com.singer.kafka.Producer;
 import com.singer.util.InputQueryUtil;
@@ -29,6 +30,9 @@ public class CommonExceptionHandler {
 	public ModelAndView defaultExceptionHandler(HttpServletRequest request, Exception ext) {
 		boolean isAjax = false;
 		log.info("defaultException");
+		if (CommonUtil.isNull(ext.getMessage())) {
+			return null;
+		}
 		log.info(ext.getMessage());
 
 		InputQueryUtil queryUtil = new InputQueryUtil("log_error");
