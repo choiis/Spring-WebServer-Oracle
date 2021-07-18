b8cc2b67c47cdc20be7fa70a69d4b6ed3a0fda86
insung choi <choiis1207@gmail.com>
2021-07-18 10:48:24 +0900

    20210718 webflux controller

--- src/main/java/com/singer/controller/WebfluxController.java
+++ src/main/java/com/singer/controller/WebfluxController.java
@@ -1,8 +1,11 @@
 package com.singer.controller;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Map.Entry;
 import java.util.stream.Stream;
 
+import org.springframework.data.domain.Range;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,27 +19,54 @@ import reactor.core.publisher.Mono;
 @AllArgsConstructor
 public class WebfluxController {
 
-	@GetMapping("/sseFlux")
+	@GetMapping("/sse/Flux")
 	public Flux<Object> sse() {
 		Stream<Integer> stream = Stream.iterate(0, i -> i + 1);
 		return Flux.fromStream(stream.limit(10)).map(i -> Collections.singletonMap("value", i));
 	}
 
-	private ReactiveRedisOperations<String, Object> personOps;
+	@GetMapping("/sse/FluxString")
+	public Flux<String> ssestring() {
+		String[] str = new String[20];
+		String strfix = "str";
+		for (int i = 0; i < 20; i++) {
+			str[i] = strfix + (i + 1);
+		}
+		Stream<String> stream = Arrays.stream(str);
+		return Flux.fromStream(stream.limit(10)).map(st -> st);
+	}
 
-	@GetMapping("/redis/man")
-	public Mono<Object> redisMan() {
-		return personOps.opsForValue().get("man");
+	private ReactiveRedisOperations<String, Object> redisOps;
+
+	@GetMapping("/redis/get/{param}")
+	public Mono<Object> redisGet(@PathVariable String param) {
+		return redisOps.opsForValue().get(param);
+	}
+
+	@GetMapping("/redis/sorted/{param}")
+	public Flux<Object> redisSorted(@PathVariable String param) {
+		Range<Long> range = Range.closed(new Long(0), new Long(-1));
+		return redisOps.opsForZSet().reverseRange(param, range);
+	}
+
+	@GetMapping("/redis/hash/{param}")
+	public Flux<Entry<Object, Object>> redishash(@PathVariable String param) {
+		return redisOps.opsForHash().entries(param);
+	}
+
+	@GetMapping("/redis/list/{param}")
+	public Flux<Object> redislist(@PathVariable String param) {
+		return redisOps.opsForList().range(param, 0, -1);
 	}
 
 	@GetMapping("/redis/keyType")
 	public Flux<Object> keyType() {
-		return personOps.keys("*").flatMap(key -> personOps.type(key));
+		return redisOps.keys("*").flatMap(key -> redisOps.type(key));
 	}
 
 	@GetMapping("/redis/keys")
 	public Flux<Object> keys() {
-		return personOps.keys("*").flatMap(key -> personOps.scan());
+		return redisOps.keys("*").flatMap(key -> redisOps.scan());
 	}
 
 }
