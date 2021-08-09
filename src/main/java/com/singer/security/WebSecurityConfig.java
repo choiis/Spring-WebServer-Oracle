cd776892575190169918a8db58ab0d348b7c9f23
insung choi <choiis1207@gmail.com>
2021-08-09 13:25:22 +0900

    20210809 spring security 버전업

--- src/main/java/com/singer/security/WebSecurityConfig.java
+++ src/main/java/com/singer/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.singer.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -8,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
@@ -15,18 +18,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private AuthenticationSuccessHandler successHandler;
-	
+
 	@Autowired
 	private AuthenticationFailureHandler failureHandler;
-	
+
 	@Autowired
 	private AuthenticationProvider authenticationProvider;
-	
+
+	@Bean
+	public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+		StrictHttpFirewall firewall = new StrictHttpFirewall();
+		firewall.setAllowBackSlash(true);
+		firewall.setAllowUrlEncodedDoubleSlash(true);
+		return firewall;
+	}
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring()
-		.antMatchers("/", "/resources/**", "/logout", "/sm**", "/main", "/sb**", "/sb**", "/sf**",
-				"/sr**", "/sv**", "/chat/**", "/comm/*", "/index");
+		super.configure(web);
+		web.httpFirewall(allowUrlEncodedSlashHttpFirewall()).ignoring().antMatchers("/", "/resources/**", "/logout",
+				"/sm**", "/main", "/sb**", "/sb**", "/sf**", "/sr**", "/sv**", "/chat/**", "/comm/*", "/index");
 
 	}
 
@@ -34,20 +45,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 
-				//.antMatchers("/", "/sm**", "/main", "/sb**", "/sb**", "/sf**", "/sr**", "/sv**", "/chat/**", "/comm/*")
-		.antMatchers("/main", "/index")		
-		.permitAll()
-		.and()
-		.csrf().disable()
+				// .antMatchers("/", "/sm**", "/main", "/sb**", "/sb**", "/sf**", "/sr**",
+				// "/sv**", "/chat/**", "/comm/*")
+				.antMatchers("/main", "/index").permitAll().and().csrf().disable()
 
 				.formLogin().loginPage("/").loginProcessingUrl("/login").failureForwardUrl("/")
-				.usernameParameter("userid").passwordParameter("passwd")
-				.successHandler(successHandler)
-				.failureHandler(failureHandler)
-				.and().authenticationProvider(authenticationProvider)
-				
-				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logoutUrl","POST")).logoutSuccessUrl("/index")
-				.invalidateHttpSession(true).permitAll();
+				.usernameParameter("userid").passwordParameter("passwd").successHandler(successHandler)
+				.failureHandler(failureHandler).and().authenticationProvider(authenticationProvider)
+
+				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logoutUrl", "POST"))
+				.logoutSuccessUrl("/index").invalidateHttpSession(true).permitAll();
 
 	}
 
